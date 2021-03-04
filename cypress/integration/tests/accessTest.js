2200e2b4569c8d03df4d946667a51c80d53d07d8
Oleg Andreyev <oandreyev.1@gmail.com>
2021-03-04 15:42:15 -0600

    init

--- cypress/integration/tests/accessTest.js
+++ cypress/integration/tests/accessTest.js
@@ -1,13 +1,23 @@
 /// <reference types="cypress" />
 
 context('Page Check', function () {
-  
-    beforeEach('Should navigate to login page', () => {
-        cy.visit('https://www.queriesoflife.com')
+    it('Zillow', () => {
+        cy.visit('https://www.zillow.com/homedetails/3109-Golden-Valley-Rd-Golden-Valley-MN-55422/1850402_zpid/')
+        cy.title().should('contains', '3109')
     })
-
-    it('Get Title', () => {
-        cy.title().should('eq', 'Queries of Life')
+    it('Redfin', () => {
+        cy.visit('https://www.redfin.com/MN/Minneapolis/3109-Golden-Valley-Rd-55422/home/50152075')
+        cy.title().should('contains', '3109')
     })
-
+    it('MLS', () => {
+        cy.visit('https://matrix.northstarmls.com/Matrix/Public/Portal.aspx?ID=DE-317521414739&eml=b2FuZHJleWV2LjFAZ21haWwuY29t#1')
+        cy.title().should('contains', 'Matrix Portal')
+    })
+    it('Realtor', () => {
+        cy.visit('https://www.realtor.com/realestateandhomes-detail/3109-Golden-Valley-Rd_Golden-Valley_MN_55422_M71725-02341', { force: true,  failOnStatusCode: false})
+    })
+    it('Trulia', () => {
+        cy.visit('https://www.trulia.com/p/mn/golden-valley/3109-golden-valley-rd-golden-valley-mn-55422--2055714019')
+        cy.title().should('contains', '3109')
+    })
 })
